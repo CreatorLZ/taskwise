967a1eeefbf82280bb22e9709013c8ba4c49d85d
Isaac Chimaroke <isaacanyim3@gmail.com>
2025-03-01 09:20:16 +0100

    typo in google login server endpoint fixed

--- server/src/routes/googleAuthRoutes.ts
+++ server/src/routes/googleAuthRoutes.ts
@@ -7,7 +7,7 @@ import {
 const router = Router();
 
 // Google authentication routes
-router.post("/google", googleLogin);
+router.post("/googlelogin", googleLogin);
 router.post("/google/callback", googleCallback);
 
 export default router;
