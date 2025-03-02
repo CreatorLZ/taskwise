406d6c6ab5f0066ac6d4e61a7452a47bd2d42519
Isaac Chimaroke <isaacanyim3@gmail.com>
2025-03-02 13:50:25 +0100

    stuf

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -2,8 +2,8 @@ import useAuthStore from "@/store/authstore";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // for local development
-  // baseURL: "https://taskwise-wibu.onrender.com/api",
+  // baseURL: "http://localhost:3000/api", // for local development
+  baseURL: "https://taskwise-wibu.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
